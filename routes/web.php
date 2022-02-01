abc89fc485cd938dd0820acfcfdb0935482e53a2
SyafiqTM <syafiq.zahir94@gmail.com>
2022-02-01 14:05:29 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', [OrderController::class,'index']);
+Route::get('/', [OrderController::class,'index'])->name('home');
